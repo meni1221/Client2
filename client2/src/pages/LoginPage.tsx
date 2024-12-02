2ad367ca243a65b07afe760b4ad2973c1e572ac3
meni_levi <1224tap@gmail.com>
2024-12-02 11:54:46 +0200

    Created login

---
+++ client2/src/pages/LoginPage.tsx
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
+export default function LoginPage() {
+  const authContext = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handelSbmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await authContext?.login({ email, password });
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    if (id === "email") setEmail(value);
+    if (id === "password") setPassword(value);
+  };
+
+  return (
+    <form onSubmit={handelSbmit}>
+      <label htmlFor="email">email</label>
+      <input 
+      type="email" 
+      id="email"
+      placeholder="Please enter an email"
+      value={email} 
+      onChange={handleInputChange}
+      required
+      />
+      <label htmlFor="password">password</label>
+      <input 
+      type="password" 
+      id="password"
+      placeholder="Please enter an password"
+      value={password} 
+      onChange={handleInputChange}
+      required
+      />
+
+        <button type="submit">LOGIN</button>
+    </form>
+  );
+}
