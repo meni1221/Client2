20ba75c3c89125ec12c33b64e247f50dc56ab271
meni_levi <1224tap@gmail.com>
2024-12-02 11:54:15 +0200

    Created layut

---
+++ client2/src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import Main from "./main";
+
+interface Props {
+    children: React.ReactNode;
+}
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Main children={children} />
+    </>
+  );
+}
