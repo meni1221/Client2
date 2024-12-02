be38865c962b9dd5d11d78253330d10912f3a5a1
meni_levi <1224tap@gmail.com>
2024-12-02 14:21:02 +0200

    Add route from admin

--- client2/src/router/AppRouter.tsx
+++ client2/src/router/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
+import AdminPage from '../pages/AdminPage'
 
 export default function AppRouter() {
   return (
     <div>
     <Routes>
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/admin' element={<AdminPage/>}/>
     </Routes>
     </div>
   )
