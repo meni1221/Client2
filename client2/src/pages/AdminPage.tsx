2a7aa45ee18766641ec2378f929d0a474c2733ab
meni_levi <1224tap@gmail.com>
2024-12-02 17:47:27 +0200

    Calling all components

--- client2/src/pages/AdminPage.tsx
+++ client2/src/pages/AdminPage.tsx
@@ -1,46 +1,20 @@
-import { useEffect, useState } from "react";
-import useFetch from "../hooks/useFetch";
-import { User } from "../interface/user";
 import UserComponemt from "../components/UserComponent";
-import BusComponent from "../components/busComponent";
-import { Bus } from "../interface/bus";
-
+import BusComponent from "../components/BusComponent";
+import LineComponent from "../components/lineComponent";
 export default function AdminPage() {
-  const { data, GET } = useFetch("http://localhost:7891/users");
-  const [users, setUsers] = useState<User[]>([]);
-  const [buses, setBuses] = useState<Bus[]>([]);
-
-  useEffect(() => {
-    GET();
-  }, []);
-
-  useEffect(() => {
-    if (data) setUsers(data);
-    // if (data) setBuses(data);
-    else console.log("not users");
-  }, [data]);
-
-
   return (
     <>
       <div>
-        {users && users.length > 0 ? (
-          users.map((user) => {
-            return <UserComponemt userProps={user} />;
-          })
-        ) : (
-          <p> not users </p>
-        )}
+        <UserComponemt />
       </div>
-
       <div>
-        {buses && buses.length > 0 ? (
-          buses.map((bus) => {
-            return <BusComponent BusProps={bus} />;
-          })
-        ) : (
-          <p> not buses </p>
-        )}
+
+        <BusComponent />
+
+      </div>
+      <div>
+
+        <LineComponent/>
       </div>
     </>
   );
