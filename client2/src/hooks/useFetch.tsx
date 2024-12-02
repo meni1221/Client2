8d2261ea351e0ec11d987f8d02a267ceafed88cd
meni_levi <1224tap@gmail.com>
2024-12-02 11:53:53 +0200

    Created GET, POST, PATCH

---
+++ client2/src/hooks/useFetch.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+export default function useFetch<T>(url: string): any {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  //   -----------GET-----------
+  const GET = async () => {
+    try {
+      const response = await fetch(`${url}`);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`HTTP error: ${errorData.error.message}`);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError((error as Error).message || "Unknown error");
+    }
+  };
+
+  //   -----------POST-----------
+  const POST = async (endpoint: string, body: object) => {
+    try {
+      const response = await fetch(`${url}/${endpoint}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error.message || "Request failed");
+      }
+      const result = await response.json();
+      setData(result);
+      return result;
+    } catch (error) {
+      setError((error as Error).message || "Unknown error");
+      throw error;
+    }
+  };
+
+  //   -----------PATCH-----------
+  const PATCH = async (id: string, body: any | object) => {
+    try {
+      const response = await fetch(`${url}/${id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include",
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(`error is:${errorData.error.message} `);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error :unknown) {
+      setError((error as Error).message || "Unknown error");
+      throw error;
+    }
+  };
+
+  return { data, error, GET, POST, PATCH };
+}
