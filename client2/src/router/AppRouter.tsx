4b7950b775653e93093250fadf39eaee8d2fa3d2
meni_levi <1224tap@gmail.com>
2024-12-03 14:45:31 +0200

    Add route from Register

--- client2/src/router/AppRouter.tsx
+++ client2/src/router/AppRouter.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import AdminPage from '../pages/AdminPage'
+import RegisterPage from '../pages/RegisterPage'
 
 export default function AppRouter() {
   return (
     <div>
     <Routes>
+        <Route path='/register' element={<RegisterPage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/admin' element={<AdminPage/>}/>
     </Routes>
