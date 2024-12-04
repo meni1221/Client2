a18f66dd4b39bc410558f4551230f6ee67f10172
meni_levi <1224tap@gmail.com>
2024-12-04 17:01:32 +0200

    route from header

--- client2/src/layout/Layout.tsx
+++ client2/src/layout/Layout.tsx
@@ -1,15 +1,16 @@
 import Footer from "./Footer";
+import Header from "./Header";
 import Main from "./main";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 export default function Layout({ children }: Props) {
   return (
     <>
+      <Header />
       <Main children={children} />
-      <Footer/>
-      
+      <Footer />
     </>
   );
 }
