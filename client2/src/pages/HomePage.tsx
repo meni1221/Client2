f4cb244b2aeb8c92e00809a8f953fe0b0d7218ac
meni_levi <1224tap@gmail.com>
2024-12-04 17:19:11 +0200

    add div

--- client2/src/pages/HomePage.tsx
+++ client2/src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-const navegate = useNavigate()
-  
+  const navegate = useNavigate();
+
   const handelSbmit = (e: React.FormEvent) => {
     e.preventDefault();
 
@@ -11,10 +11,10 @@ const navegate = useNavigate()
   };
   return (
     <>
-    <h2>אתה לא מחובר אנא צור חיבור</h2>
-    <button onClick={handelSbmit}>
-      לעמוד לוגין
-    </button>
+      <div className="Home">
+        <h2>אחי אתה לא מחובר</h2>
+        <button onClick={handelSbmit}>לעמוד לוגין</button>
+      </div>
     </>
-  )
+  );
 }
